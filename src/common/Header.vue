68abcc1d385ae1f6e572c576763892b87ff8a7b5
timurkartaev <timurkartaev@gmail.com>
2018-05-02 07:09:01 +0000

    header container wrapped with container class

--- src/common/Header.vue
+++ src/common/Header.vue
@@ -1,31 +1,35 @@
 <template>
     <nav class="navbar navbar-expand-md navbar-dark bg-danger">
-        <router-link class="navbar-brand" to="/">Vue Hacker news</router-link>
-        <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarText"
-            aria-controls="navbarText"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-        </button>
+        <div class="container">
+                
+            <router-link class="navbar-brand" to="/">Vue Hacker news</router-link>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarText"
+                aria-controls="navbarText"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div id="navbarText" class="collapse navbar-collapse justify-content-stretch">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/post-form">submit</router-link>
-                </li>
-            </ul>
+            <div id="navbarText" class="collapse navbar-collapse justify-content-stretch">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/post-form">submit</router-link>
+                    </li>
+                </ul>
 
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/log-in">login</router-link>
-                </li>
-            </ul>
-            
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/log-in">login</router-link>
+                    </li>
+                </ul>
+                
+            </div>
         </div>
+        
     </nav>
 </template>
 
