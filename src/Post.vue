679e7162ddc8fefa4e9f78b3f85f463e92391852
timurkartaev <timurkartaev@gmail.com>
2018-05-02 06:56:38 +0000

    added post detail parts

--- src/Post.vue
+++ src/Post.vue
@@ -1,7 +1,8 @@
 <template>
     <li @click="postClick(link)">
-        <slot name="number"></slot>
+        <slot name="number"></slot> <span class="upvote">&#9650;</span>
         <slot name="title"></slot>
+        <slot name="details"></slot>
     </li>
 </template>
 
@@ -16,3 +17,10 @@
         }
     }
 </script>
+
+<style scoped>
+    .details, .link, .number, .upvote { color: #999; }
+    .link, .details { font-size: 0.75rem; }
+    .title { color: #424242; }
+
+</style>
--- src/PostList.vue
+++ src/PostList.vue
@@ -3,12 +3,15 @@
         <div class="row">
             <div class="col">
                 <ul class="list-unstyled">
-                    <app-post v-for="(post, index) in posts" :link="post.link">
+                    <app-post v-for="(post, index) in posts" :link="post.link" :key="post.id">
                         <template slot="number">
-                            {{ index +1 }}
+                            <span class="number">{{ index +1 }}.</span>
                         </template>
                         <template slot="title">
-                            {{ post.title }}
+                            <a class="title" :href="post.link">{{ post.title }}</a> <span class="link">{{ post.link }}</span>
+                        </template>
+                        <template slot="details">
+                            <p class="details">{{ post.points }} points by {{ post.author }} | {{ post.comments }} comments</p>
                         </template>
                     </app-post>
                 </ul>
@@ -25,6 +28,7 @@
             return {
                 posts: [
                     {
+                        id: 1,
                         title: 'Medium tries to prevent people reading deleted articles on the Wayback Machine',
                         points: 151,
                         link: 'https://google.com',
@@ -33,6 +37,7 @@
                     },
 
                     {
+                        id: 2,
                         title: 'You know how HTTP GET requests are meant to be idempotent?',
                         points: 151,
                         link: 'https://google.com',
