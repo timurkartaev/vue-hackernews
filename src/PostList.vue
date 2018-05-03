0361f0da0d74bb9424a2381bbd63009c9eb4955f
timurkartaev <timurkartaev@gmail.com>
2018-05-03 06:46:53 +0000

    added vue resource fetching data from algolia integrated get request during created component life cycle

--- src/PostList.vue
+++ src/PostList.vue
@@ -3,16 +3,16 @@
         <div class="row">
             <div class="col">
                 <ul class="list-unstyled">
-                    <app-post v-for="(post, index) in posts" :link="post.link" :key="post.id">
+                    <app-post v-for="(post, index) in posts" :key="index">
                         <template slot="post">
                             <div class="d-flex mb-2">
                                 <div class="p-2">
-                                    <span class="number">{{ index +1 }}. &#9650;</span>
+                                    <span class="number">{{ index +1 }}.</span>
                                 </div>
                                 
                                 <div class="p-2">
-                                    <a class="title" :href="post.link">
-                                        {{ post.title }} <span class="link">{{ post.link }}</span>
+                                    <a class="title" :href="post.url">
+                                        {{ post.title }} <span class="link">({{ post.url }})</span>
                                     </a> 
                                     <p class="details">{{ post.points }} points by {{ post.author }} | {{ post.comments }} comments</p></span></div>
                             </div>
@@ -30,30 +30,32 @@
     export default {
         data() {
             return {
-                posts: [
-                    {
-                        id: 1,
-                        title: 'Medium tries to prevent people reading deleted articles on the Wayback Machine',
-                        points: 151,
-                        link: 'https://google.com',
-                        author: 'scandox',
-                        comments: 76,
-                    },
-
-                    {
-                        id: 2,
-                        title: 'You know how HTTP GET requests are meant to be idempotent?',
-                        points: 151,
-                        link: 'https://google.com',
-                        author: 'scandox',
-                        comments: 76
-                    }
-
-                ]
+                posts: []
             }
+        },
+        methods: {
         },
         components: {
             appPost: Post
+        },
+        created() {
+            this.$http.get('https://hn.algolia.com/api/v1/search',
+                          {'tags': 'story',
+                           'hitsPerPage': 50})
+                      .then(response => {
+                        return response.json();
+                      }, error => {
+
+                      })
+                      .then(json_data => {
+                          for( let item in json_data['hits']) {
+                            if ( json_data['hits'][item]['title']) {
+                                console.log(json_data['hits'][item]);
+                                this.posts.push(json_data['hits'][item]);
+                            }
+                          }
+                          console.log(this.posts);
+                      });
         }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import { routes } from './routes';
 import App from './App.vue'
 
+Vue.use(VueResource);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
